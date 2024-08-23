30b142fe63fc796df306acf0d447ba1c30492b09
Flypans <sbkwon011@outlook.com>
2024-08-23 10:03:36 +0100

    Fix: Varible name conflict

--- ToDoList/ToDoList.ConsoleApp/Program.cs
+++ ToDoList/ToDoList.ConsoleApp/Program.cs
@@ -8,15 +8,27 @@ namespace ToDoList.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //Test
             IToDoRepositoryInMemory _repository = new ToDoRepositoryInMemory();
             List<ToDo> toDos = new List<ToDo>();
+
+            //CRUD(Read)
             toDos = _repository.GetAll();
-            foreach (var todo in toDos)
+            foreach (var t in toDos)
             {
-                Console.WriteLine($"ID: {todo.Id}, Title: {todo.Title}, is done: {todo.IsDone}");
+                Console.WriteLine($"ID: {t.Id}, Title: {t.Title}, is done: {t.IsDone}");
             }
-             
+            Console.WriteLine("Default Database");
+
+            //CRUD(Create)
+            ToDo todo = new ToDo {Title = "SQL", IsDone = true };
+            _repository.Add(todo);
+
+            //CRUD(Read)
+            toDos = _repository.GetAll();
+            foreach (var t in toDos)
+            {
+                Console.WriteLine($"ID: {t.Id}, Title: {t.Title}, is done: {t.IsDone}");
+            }
         }
     }
 }
